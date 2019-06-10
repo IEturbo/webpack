cd312981e266cb5fe45a8c918a3f3b25d185438a
IEturbo <ghb_918@163.com>
2019-06-10 17:04:49 +0800

    refactor code

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [{
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }, {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'file-loader',
-        }]
-      }, {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }, {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
   }
 }
